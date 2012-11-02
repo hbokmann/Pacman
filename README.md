148f80633c8cf2d6a099915d5a9f5647ddbade72
Hans Bokmann <hans@bokmann.com>
2012-11-02 02:31:36 +0200

    Walls with collision detection
    Walls with collision detection
    Also Pacman image

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### Pacman in Python with PyGame
 
-![Pacman Game Window](https://raw.github.com/hbokmann/Pacman/master/pacman.png)
+![Pacman Game Window](https://raw.github.com/hbokmann/Pacman/master/pacman.jpg)
 
 GitHub Project Page: https://github.com/hbokmann/Pacman
--- pacman.py
+++ pacman.py
@@ -1,21 +1,21 @@
 # Sample Python/Pygame Programs
 # Simpson College Computer Science
 # http://cs.simpson.edu
- 
+  
 import pygame
- 
-# Define some colors
-black    = (   0,   0,   0)
-white    = ( 255, 255, 255)
-green    = (   0, 255,   0)
-red      = ( 255,   0,   0)
-blue     = (   0,   0, 255)
+  
+black = (0,0,0)
+white = (255,255,255)
+blue = (0,0,255)
+green = (0,255,0)
+red = (255,0,0)
+purple = (255,0,255)
 yellow   = ( 255, 255,   0)
+
 
 def pacman(screen, x, y):
     color = yellow
     pygame.draw.ellipse(screen,color,[x,y,30,30])
-
 
 # This class represents the bar at the bottom that the player controls
 class Wall(pygame.sprite.Sprite):
@@ -32,7 +32,56 @@ class Wall(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.top = y
         self.rect.left = x
-
+          
+# This class represents the bar at the bottom that the player controls
+class Player(pygame.sprite.Sprite):
+  
+    # Set speed vector
+    change_x=0
+    change_y=0
+  
+    # Constructor function
+    def __init__(self,x,y, filename):
+        # Call the parent's constructor
+        pygame.sprite.Sprite.__init__(self)
+   
+        # Set height, width
+        self.image = pygame.image.load(filename).convert()
+  
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.top = y
+        self.rect.left = x
+      
+    # Change the speed of the player
+    def changespeed(self,x,y):
+        self.change_x+=x
+        self.change_y+=y
+          
+    # Find a new position for the player
+    def update(self,walls):
+        # Get the old position, in case we need to go back to it
+        old_x=self.rect.left
+        new_x=old_x+self.change_x
+        self.rect.left = new_x
+          
+        # Did this update cause us to hit a wall?
+        collide = pygame.sprite.spritecollide(self, walls, False)
+        if collide:
+            # Whoops, hit a wall. Go back to the old position
+            self.rect.left=old_x
+  
+        old_y=self.rect.top
+        new_y=old_y+self.change_y
+        self.rect.top = new_y
+          
+        # Did this update cause us to hit a wall?
+        collide = pygame.sprite.spritecollide(self, walls, False)
+        if collide:
+            # Whoops, hit a wall. Go back to the old position
+            self.rect.top=old_y
+  
+ 
 # This creates all the walls in room 1
 def setupRoomOne():
     # Make the walls. (x_pos, y_pos, width, height)
@@ -86,115 +135,90 @@ def setupRoomOne():
      
     # return our new list
     return wall_list
-
-current_room = 1
-wall_list = setupRoomOne()
-
-# This sets the width and height of each grid location
-width=54
-height=54
- 
-# This sets the margin between each cell
-margin=6
- 
-# Create a 2 dimensional array. A two dimesional
-# array is simply a list of lists.
-grid=[]
-for row in range(10):
-    # Add an empty array that will hold each cell
-    # in this row
-    grid.append([])
-    for column in range(10):
-        grid[row].append(0) # Append a cell
-
-# Initialize pygame
+  
+score = 0
+# Call this function so the Pygame library can initialize itself
 pygame.init()
   
-# Set the height and width of the screen
-size=[606,606]
-screen=pygame.display.set_mode(size)
- 
-# Set title of screen
-pygame.display.set_caption("Pacman")
- 
-#Loop until the user clicks the close button.
-done=False
- 
-# Used to manage how fast the screen updates
-clock=pygame.time.Clock()
-    
-# Hide the mouse cursor
-pygame.mouse.set_visible(0)
-
-# Speed in pixels per frame
-x_speed=0
-y_speed=0
+# Create an 600x600 sized screen
+screen = pygame.display.set_mode([606, 606])
   
-# Current position
-x_coord=18
-y_coord=18
-
-
-# -------- Main Program Loop -----------
-while done==False:
+# Set the title of the window
+pygame.display.set_caption('Pac-man')
+  
+# Create a surface we can draw on
+background = pygame.Surface(screen.get_size())
+  
+# Used for converting color maps and such
+background = background.convert()
+  
+# Fill the screen with a black background
+background.fill(black)
+  
+# Create the player paddle object
+player = Player( 378, 138, "pacman.png" )
+movingsprites = pygame.sprite.RenderPlain()
+movingsprites.add(player)
+  
+current_room = 1
+wall_list = setupRoomOne()
+ 
+clock = pygame.time.Clock()
+  
+done = False
+  
+while done == False:
     # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
-            # User pressed down on a key
-         
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done=True
+  
         if event.type == pygame.KEYDOWN:
-            # Figure out if it was an arrow key. If so
-            # adjust speed.
             if event.key == pygame.K_LEFT:
-                x_speed=-60
+                player.changespeed(-30,0)
             if event.key == pygame.K_RIGHT:
-                x_speed=60
+                player.changespeed(30,0)
             if event.key == pygame.K_UP:
-                y_speed=-60
+                player.changespeed(0,-30)
             if event.key == pygame.K_DOWN:
-                y_speed=60
+                player.changespeed(0,30)
                   
-        # User let up on a key
         if event.type == pygame.KEYUP:
-            # If it is an arrow key, reset vector back to zero
             if event.key == pygame.K_LEFT:
-                x_speed=0
+                player.changespeed(30,0)
             if event.key == pygame.K_RIGHT:
-                x_speed=0
+                player.changespeed(-30,0)
             if event.key == pygame.K_UP:
-                y_speed=0
+                player.changespeed(0,30)
             if event.key == pygame.K_DOWN:
-                y_speed=0
+                player.changespeed(0,-30)
     # ALL EVENT PROCESSING SHOULD GO ABOVE THIS COMMENT
  
     # ALL GAME LOGIC SHOULD GO BELOW THIS COMMENT
-    wall_list = setupRoomOne()
-    current_room = 1
-            
-    # Move the object according to the speed vector.
-    x_coord=x_coord+x_speed
-    y_coord=y_coord+y_speed
+    player.update(wall_list)
+
+    # ALL GAME LOGIC SHOULD GO ABOVE THIS COMMENT
  
-    # Set the screen background
+                  
+    # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
     screen.fill(black)
-
-    pacman(screen,x_coord,y_coord)
-
+      
+    movingsprites.draw(screen)
     wall_list.draw(screen)
+    # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
     
     # Draw the grid
     for row in range(10):
         for column in range(10):
             color = yellow
-            pygame.draw.ellipse(screen,color,[((margin+width)*column+margin)+24,((margin+height)*row+margin)+24,6,6])
+            if row == 4 and (column == 4 or column == 5):
+                continue
+            else:
+                pygame.draw.ellipse(screen,color,[(60*column+6)+24,(60*row+6)+24,6,6])
  
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-    
-    # Limit to 20 frames per second
-    clock.tick(10)
 
-# Be IDLE friendly. If you forget this line, the program will 'hang'
-# on exit.
-pygame.quit ()
+    pygame.display.flip()
+  
+    clock.tick(10)
+              
+pygame.quit()
